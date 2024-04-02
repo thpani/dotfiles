fa5cf410d66c7205f17052b937326a200ed12636
Thomas Pani <thomas.pani@gmail.com>
2024-04-02 10:45:59 +0200

    Comment out debug code

--- hammerspoon_init.lua
+++ hammerspoon_init.lua
@@ -13,14 +13,14 @@ hs.hotkey.bind({"option"}, "space", function()
   end
 end)
 
-DEBUG_TAP = false
+-- DEBUG_TAP = false
 tap = hs.eventtap.new({hs.eventtap.event.types.systemDefined}, function(event)
-  if DEBUG_TAP then
-    print("event tap debug got event:")
-    print(hs.inspect.inspect(event:getRawEventData()))
-    print(hs.inspect.inspect(event:getFlags()))
-    print(hs.inspect.inspect(event:systemKey()))
-  end
+  -- if DEBUG_TAP then
+  --   print("event tap debug got event:")
+  --   print(hs.inspect.inspect(event:getRawEventData()))
+  --   print(hs.inspect.inspect(event:getFlags()))
+  --   print(hs.inspect.inspect(event:systemKey()))
+  -- end
   local sys_key_event = event:systemKey()
   if not sys_key_event or not sys_key_event.down or sys_key_event['repeat'] then
     return false -- do not delete event
