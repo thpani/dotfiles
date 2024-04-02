ab48d03e49595dfc3b3d94b44891958de75797c2
Thomas Pani <thomas.pani@gmail.com>
2024-04-02 10:45:44 +0200

    Retab

--- hammerspoon_init.lua
+++ hammerspoon_init.lua
@@ -15,20 +15,20 @@ end)
 
 DEBUG_TAP = false
 tap = hs.eventtap.new({hs.eventtap.event.types.systemDefined}, function(event)
-	if DEBUG_TAP then
-		print("event tap debug got event:")
-		print(hs.inspect.inspect(event:getRawEventData()))
-		print(hs.inspect.inspect(event:getFlags()))
-		print(hs.inspect.inspect(event:systemKey()))
-	end
-	local sys_key_event = event:systemKey()
-	if not sys_key_event or not sys_key_event.down or sys_key_event['repeat'] then
-		return false -- do not delete event
+  if DEBUG_TAP then
+    print("event tap debug got event:")
+    print(hs.inspect.inspect(event:getRawEventData()))
+    print(hs.inspect.inspect(event:getFlags()))
+    print(hs.inspect.inspect(event:systemKey()))
   end
-	if sys_key_event.key == "PLAY" then
+  local sys_key_event = event:systemKey()
+  if not sys_key_event or not sys_key_event.down or sys_key_event['repeat'] then
+    return false -- do not delete event
+  end
+  if sys_key_event.key == "PLAY" then
     hs.spotify.playpause()
-		return true
-	end
-	return false
+    return true
+  end
+  return false
 end)
 tap:start()
